cdc034b547f501a92abffdf8f7e563368e6fdb30
satyasgithub <35933766+satyasgithub@users.noreply.github.com>
2018-02-07 01:50:45 -0500

    skills challenge
    Added few lines of code with rest web service

---
+++ coding challenge/SStone/src/com/sstone/harish/rest/MyServices.java
@@ -0,0 +1,13 @@
+package com.sstone.harish.rest;
+ 
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rs")
+public class MyServices extends ResourceConfig 
+{
+    public MyServices() {
+        packages("com.fasterxml.jackson.jaxrs.json");
+        packages("com.sstone.harish.rest");
+    }
+}
